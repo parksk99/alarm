48555586371607815c99246b5d4bc191444c0346
parksk99 <qkrtjdrb99@naver.com>
2021-03-15 17:42:47 +0900

    timePicker에서 시간을 잘 가져오는지 toast로 찍어

--- app/src/main/java/com/example/alarm/AlarmReceiver.java
+++ app/src/main/java/com/example/alarm/AlarmReceiver.java
@@ -1,34 +1,72 @@
 package com.example.alarm;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.widget.Toast;
 
 import androidx.core.app.NotificationCompat;
 
 public class AlarmReceiver extends BroadcastReceiver {
-    private NotificationManager notificationManager;
-    private NotificationCompat.Builder builder;
+    public AlarmReceiver(){ }
+
+    NotificationManager manager;
+    NotificationCompat.Builder builder;
+
+    //오레오 이상은 반드시 채널을 설정해줘야 Notification이 작동함
+    private static String CHANNEL_ID = "channel1";
+    private static String CHANNEL_NAME = "Channel1";
+
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        Toast toast = Toast.makeText(context, "hi2", Toast.LENGTH_LONG);
-        toast.show();
-        notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-        Intent notificationIntent = new Intent(context, MainActivity.class);
-        PendingIntent notificationPendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        builder = new NotificationCompat.Builder(context,"")
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle(intent.getStringExtra("contentTitle"))
-                .setContentText(intent.getStringExtra("contentText"))
-                .setDefaults(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(notificationPendingIntent)
-                .setAutoCancel(true);
-        notificationManager.notify(100, builder.build());
+        AlarmManager am = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+
+        builder = null;
+        manager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            manager.createNotificationChannel(
+                    new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+            );
+            builder = new NotificationCompat.Builder(context, CHANNEL_ID);
+        }
+
+        //알림창 클릭 시 activity 화면 부름
+        Intent intent2 = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,101,intent2, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+        builder.setContentTitle("알람");
+        builder.setSmallIcon(R.drawable.ic_launcher_background);
+        builder.setAutoCancel(true);
+        builder.setContentIntent(pendingIntent);
+        Notification notification = builder.build();
+        manager.notify(1,notification);
+
     }
 }
+
+
+
+//        notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+//        Intent notificationIntent = new Intent(context, MainActivity.class);
+//        PendingIntent notificationPendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+//        builder = new NotificationCompat.Builder(context,"")
+//                .setSmallIcon(R.drawable.ic_launcher_background)
+//                .setContentTitle(intent.getStringExtra("contentTitle"))
+//                .setContentText(intent.getStringExtra("contentText"))
+//                .setDefaults(NotificationCompat.PRIORITY_DEFAULT)
+//                .setContentIntent(notificationPendingIntent)
+//                .setAutoCancel(true);
+//        notificationManager.notify(100, builder.build());
+
+
+
+
+
--- app/src/main/java/com/example/alarm/MainActivity.java
+++ app/src/main/java/com/example/alarm/MainActivity.java
@@ -14,6 +14,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TimePicker;
@@ -33,37 +34,62 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setTitle("박성규 맞춤 알람");
+        setTitle(" 공익 박성규 맞춤 알람 service ");
         Button alarm = findViewById(R.id.alarm);
         timePicker = findViewById(R.id.timePicker);
         timePicker.setIs24HourView(true);   //12시간 표기방법에서 24시간 표기방법으로 바꿈
         alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
-        createNotificationChannel();
+     // createNotificationChannel();
+
         alarm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+                    @Override
+                    public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                    }
+                });
                 Calendar calendar = getTimePicker();    //timePicker로부터 시간을 가져오고 그 시간을 alarmManager에게 전달
                 setAlarm(calendar);
             }
         });
-
     }
 
     private void setAlarm(Calendar calendar){
-        Toast toast = Toast.makeText(this, "hi", Toast.LENGTH_LONG);
-        toast.show();
+        //Toast toast = Toast.makeText(this, "setAlarm", Toast.LENGTH_LONG);
+      //  toast.show();
+
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Intent receiverIntent = new Intent(MainActivity.this, AlarmReceiver.class);
         receiverIntent.putExtra("contentTitle", "contentTitle");
         receiverIntent.putExtra("contentText", "contentText");
         PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, receiverIntent, 0);
+      //  PendingIntent.getService(context, requestId, intent,
+              //  PendingIntent.FLAG_NO_CREATE);
         try {
-            calendar.setTime(dateFormat.parse("2021-03-15 14:41:00"));
+            calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DATE), 12, 57, 00);
+
+          //    calendar.setTime(dateFormat.parse("2021-03-16 12:29:00"));
         }catch(Exception e){
             e.printStackTrace();
         }
-        alarmManager.set(AlarmManager.RTC, calendar.getTimeInMillis(), pendingIntent);
+
+
+
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, 13);
+        calendar.set(Calendar.MINUTE, 34);
+
+
+        // With setInexactRepeating(), you have to use one of the AlarmManager interval
+        // constants--in this case, AlarmManager.INTERVAL_DAY.
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                AlarmManager.INTERVAL_DAY, pendingIntent);
+
+
+
+    //    alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
     }
 
     //timePicker에 저장된 시간을 가져옴
@@ -75,13 +101,16 @@ public class MainActivity extends AppCompatActivity {
         //이전 버전과 호환을 위해 if로 구현
         if(Build.VERSION.SDK_INT >= 23){
             hour = timePicker.getHour();
-            min = timePicker.getMinute();
+            min = timePicker.getMinute()
+        ;
         }
         else{
             hour = timePicker.getCurrentHour();
             min = timePicker.getCurrentMinute();
         }
        // calendar.set()
+        Toast toast = Toast.makeText(this, "설정 시간 : "+hour+"시 "+min+"분", Toast.LENGTH_SHORT);
+        toast.show();
     return calendar;    //****calendar에 hour와 min을 저장해야
     }
 
@@ -102,15 +131,21 @@ public class MainActivity extends AppCompatActivity {
 //        toast.show();
 //        notificationManager.notify(100, getNotificationBuilder("title", "text").build());
 //    }
-    private void createNotificationChannel(){
-        notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-                CharSequence name = getString(R.string.channel_name);
-                int importance = NotificationManager.IMPORTANCE_DEFAULT;
-                String description = getString(R.string.channel_description);
-                NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-                channel.setDescription(description);
-                notificationManager.createNotificationChannel(channel);
-            }
-    }
+
+
+
+//    private void createNotificationChannel(){
+//        notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+//            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+//                CharSequence name = getString(R.string.channel_name);
+//                int importance = NotificationManager.IMPORTANCE_DEFAULT;
+//                String description = getString(R.string.channel_description);
+//                NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+//                channel.setDescription(description);
+//                notificationManager.createNotificationChannel(channel);
+//            }
+//    }
+
+
+
 }
