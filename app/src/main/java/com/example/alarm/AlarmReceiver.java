32d8daca0bbeab16b112350193b62bbe2eade6b5
parksk99 <qkrtjdrb99@naver.com>
2021-03-15 14:41:50 +0900

    ㄷ됐나?

---
+++ app/src/main/java/com/example/alarm/AlarmReceiver.java
@@ -0,0 +1,34 @@
+package com.example.alarm;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    private NotificationManager notificationManager;
+    private NotificationCompat.Builder builder;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Toast toast = Toast.makeText(context, "hi2", Toast.LENGTH_LONG);
+        toast.show();
+        notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder = new NotificationCompat.Builder(context,"")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle(intent.getStringExtra("contentTitle"))
+                .setContentText(intent.getStringExtra("contentText"))
+                .setDefaults(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(notificationPendingIntent)
+                .setAutoCancel(true);
+        notificationManager.notify(100, builder.build());
+    }
+}
--- app/src/main/java/com/example/alarm/MainActivity.java
+++ app/src/main/java/com/example/alarm/MainActivity.java
@@ -19,6 +19,7 @@ import android.widget.Button;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
@@ -43,11 +44,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Calendar calendar = getTimePicker();    //timePicker로부터 시간을 가져오고 그 시간을 alarmManager에게 전달
-                //alarmManager.set(AlarmManager.RTC, calendar.getTimeInMillis(), ); //마지막 인자 : pendingIntent
-                sendNotification();
+                setAlarm(calendar);
             }
         });
 
+    }
+
+    private void setAlarm(Calendar calendar){
+        Toast toast = Toast.makeText(this, "hi", Toast.LENGTH_LONG);
+        toast.show();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Intent receiverIntent = new Intent(MainActivity.this, AlarmReceiver.class);
+        receiverIntent.putExtra("contentTitle", "contentTitle");
+        receiverIntent.putExtra("contentText", "contentText");
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, receiverIntent, 0);
+        try {
+            calendar.setTime(dateFormat.parse("2021-03-15 14:41:00"));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        alarmManager.set(AlarmManager.RTC, calendar.getTimeInMillis(), pendingIntent);
     }
 
     //timePicker에 저장된 시간을 가져옴
@@ -56,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
         int hour, min;
 
         //timePicker가 23버전부터 getHour를 사용하게 함
-        //이전 버전과 호환을 위해 if로 구
+        //이전 버전과 호환을 위해 if로 구현
         if(Build.VERSION.SDK_INT >= 23){
             hour = timePicker.getHour();
             min = timePicker.getMinute();
@@ -69,30 +85,30 @@ public class MainActivity extends AppCompatActivity {
     return calendar;    //****calendar에 hour와 min을 저장해야
     }
 
-    private NotificationCompat.Builder getNotificationBuilder(){
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-        PendingIntent notificationPendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this,CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle("test")
-                .setContentText("alarm test")
-                .setDefaults(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(notificationPendingIntent)
-                .setAutoCancel(true);
-        return builder;
-    }
-    private void sendNotification(){
-        Toast toast = Toast.makeText(this, "hi", Toast.LENGTH_SHORT);
-        toast.show();
-        notificationManager.notify(100, getNotificationBuilder().build());
-    }
+//    private NotificationCompat.Builder getNotificationBuilder(String contentTitle, String contentText){
+//        Intent notificationIntent = new Intent(this, MainActivity.class);
+//        PendingIntent notificationPendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+//        NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this,CHANNEL_ID)
+//                .setSmallIcon(R.drawable.ic_launcher_background)
+//                .setContentTitle(contentTitle)
+//                .setContentText(contentText)
+//                .setDefaults(NotificationCompat.PRIORITY_DEFAULT)
+//                .setContentIntent(notificationPendingIntent)
+//                .setAutoCancel(true);
+//        return builder;
+//    }
+//    private void sendNotification(){
+//        Toast toast = Toast.makeText(this, "hi", Toast.LENGTH_SHORT);
+//        toast.show();
+//        notificationManager.notify(100, getNotificationBuilder("title", "text").build());
+//    }
     private void createNotificationChannel(){
         notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
             if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
                 CharSequence name = getString(R.string.channel_name);
                 int importance = NotificationManager.IMPORTANCE_DEFAULT;
                 String description = getString(R.string.channel_description);
-                NotificationChannel channel = new NotificationChannel("alarm", name, importance);
+                NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
                 channel.setDescription(description);
                 notificationManager.createNotificationChannel(channel);
             }
