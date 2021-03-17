9ff476d75916c0291223edbe02af3e4eece3e6b0
parksk99 <qkrtjdrb99@naver.com>
2021-03-17 10:23:46 +0900

    timePicker가 1시간씩 밀리는 버그를 대충 고

--- app/src/main/java/com/example/alarm/MainActivity.java
+++ app/src/main/java/com/example/alarm/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private NotificationManager notificationManager;
     private AlarmManager alarmManager;
     private TimePicker timePicker;
+    private int hour, min;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +39,21 @@ public class MainActivity extends AppCompatActivity {
         Button alarm = findViewById(R.id.alarm);
         timePicker = findViewById(R.id.timePicker);
         timePicker.setIs24HourView(true);   //12시간 표기방법에서 24시간 표기방법으로 바꿈
+        if(Build.VERSION.SDK_INT >= 23){
+            hour = timePicker.getHour();
+            min = timePicker.getMinute();
+        }
+        else{
+            hour = timePicker.getCurrentHour();
+            min = timePicker.getCurrentMinute();
+        }
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                hour = hourOfDay-1;
+                min = minute-1;
+            }
+        });
         alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
         class AlarmRunable implements Runnable{
@@ -51,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
         alarm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Calendar calendar = getTimePicker();    //timePicker로부터 시간을 가져오고 그 시간을 alarmManager에게 전달
+                Calendar calendar = getTime();    //timePicker로부터 시간을 가져오고 그 시간을 alarmManager에게 전달
                 setAlarm(calendar);
             }
         });
@@ -74,22 +90,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //timePicker에 저장된 시간을 가져옴
-    private Calendar getTimePicker(){
+    private Calendar getTime(){
         Calendar calendar = Calendar.getInstance();
-        int hour, min;
+//        int hour, min;
         //timePicker가 23버전부터 getHour를 사용하게 함
         //이전 버전과 호환을 위해 if로 구현
-        if(Build.VERSION.SDK_INT >= 23){
-            hour = timePicker.getHour();
-            min = timePicker.getMinute();
-        }
-        else{
-            hour = timePicker.getCurrentHour();
-            min = timePicker.getCurrentMinute();
-        }
+//        if(Build.VERSION.SDK_INT >= 23){
+//            hour = timePicker.getHour();
+//            min = timePicker.getMinute();
+//        }
+//        else{
+//            hour = timePicker.getCurrentHour();
+//            min = timePicker.getCurrentMinute();
+//        }
        // calendar.set()
-//        Toast toast = Toast.makeText(this, "설정 시간 : "+hour+"시 "+min+"분", Toast.LENGTH_SHORT);
-//        toast.show();
+        Toast toast = Toast.makeText(this, "설정 시간 : "+hour+"시 "+min+"분", Toast.LENGTH_SHORT);
+        toast.show();
     return calendar;    //****calendar에 hour와 min을 저장해야
     }
 
