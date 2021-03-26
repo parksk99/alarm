e800713b2feabe1e14ab530b4aa47c0456e88beb
parksk99 <qkrtjdrb99@naver.com>
2021-03-26 11:38:23 +0900

    삭제 어찌저찌 가능, db 기능 구축, textView 동적 생

--- app/src/main/java/com/example/alarm/ManageActivity.java
+++ app/src/main/java/com/example/alarm/ManageActivity.java
@@ -10,6 +10,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -18,11 +19,15 @@ public class ManageActivity extends AppCompatActivity {
     Button button;
     ContactDBHelper dbHelper;
     TextView textView;
+    LinearLayout linearLayout;
+    SQLiteDatabase alarmDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage);
+        linearLayout = (LinearLayout)findViewById(R.id.manage_layout);
 
+        alarmDatabase = this.openOrCreateDatabase("contact.db", MODE_PRIVATE, null);
         textView = setTextView();
         dbHelper = new ContactDBHelper(this);
         button = findViewById(R.id.button);
@@ -31,25 +36,39 @@ public class ManageActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(ManageActivity.this, AlarmReceiver.class);
                 AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE);
-                am.cancel(PendingIntent.getBroadcast(ManageActivity.this, getRequestCode(textView.getText().toString()), intent, PendingIntent.FLAG_UPDATE_CURRENT));
+                int requestCode = getRequestCode(textView.getText().toString());
+                am.cancel(PendingIntent.getBroadcast(ManageActivity.this, requestCode, intent, PendingIntent.FLAG_UPDATE_CURRENT));
                 //arrayList의 0번째 요소도 지워야 하는데 여기서 지우면 원래 있던 애도 지워지나?
+                alarmDatabase.execSQL(ContactDBCtrict.SQL_DELETE+" WHERE "+ContactDBCtrict.COL_CONTENT_TITLE+" = '"+textView.getText().toString()+"'");
             }
         });
     }
     private Cursor getCursor(){
-        SQLiteDatabase alarmDatabase = dbHelper.getReadableDatabase();
         return alarmDatabase.rawQuery(ContactDBCtrict.SQL_SELECT, null);
     }
     private TextView setTextView(){
-        TextView tmpTextView = new TextView(this);
+        TextView tmpTextView = new TextView(this);;
         Cursor cursor = getCursor();
-        tmpTextView.setText(cursor.getString(0));
+        if(cursor.moveToNext()) {
+            tmpTextView.setText(cursor.getString(0));
+            cursor.close();
+        }
+        else{
+            tmpTextView.setText("there is no alarm");
+        }
+        LinearLayout.LayoutParams parms = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        tmpTextView.setLayoutParams(parms);
+        linearLayout.addView(tmpTextView);
         return tmpTextView;
     }
     private int getRequestCode(String contentTitle){
-        SQLiteDatabase alarmDatabase = dbHelper.getReadableDatabase();
         // + " WHERE " + COL_CONTENT_TITLE + " = ";
-        Cursor cursor = alarmDatabase.rawQuery(ContactDBCtrict.SQL_SELECT+" WHERE "+ContactDBCtrict.COL_CONTENT_TITLE+" = "+contentTitle, null);
-        return cursor.getInt(2) + cursor.getString(1).hashCode();
+        int tmp = -1;
+        Cursor cursor = alarmDatabase.rawQuery(ContactDBCtrict.SQL_SELECT+" WHERE "+ContactDBCtrict.COL_CONTENT_TITLE+" = '"+contentTitle+"'", null);
+        if(cursor.moveToNext()) {
+            tmp = cursor.getInt(2) + cursor.getString(1).hashCode();
+            cursor.close();
+        }
+        return tmp;
     }
 }
